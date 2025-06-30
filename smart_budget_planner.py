2da7ee59fe80e79bb674b3b2c85d8e34946d8245
Samarth-06 <samwaghrulkar@gmail.com>
2025-06-30 20:42:43 +0530

    code

--- smart_budget_planner.py
+++ smart_budget_planner.py
@@ -1,8 +1,9 @@
-
+# importing the required libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#Define budget and categories
 budget = float(input("Enter your monthly budget: ₹"))
 
 cat = ['Rent', 'Groceries', 'Utilities', 'Transport', 'Entertainment', 'Education', 'Others']
@@ -29,16 +30,19 @@ print(data)
 print("\nTotal Spent: ₹", total)
 print("Remaining Budget: ₹", left)
 
+# Using conditional statements
 if left < 0:
     print("You have spent more than your budget 😓")
 else:
     print("Nice! You are in your budget 😄")
-
+    
+# Pie chart of category-wise spending
 plt.figure(figsize=(8, 8))
 plt.pie(data['Expense'], labels=data['Category'], autopct='%1.1f%%')
 plt.title("Where Your Money Went")
 plt.show()
 
+# Bar chart of expenses
 plt.figure(figsize=(10, 5))
 plt.bar(data['Category'], data['Expense'], color='orange')
 plt.axhline(avg, color='red', linestyle='--', label='Average Limit')
